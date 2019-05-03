d219eeeba1b8a8177fdc5843df2b3951519e5e63
alexrasla <38263534+alexrasla@users.noreply.github.com>
2019-05-02 19:42:19 -0700

    Update AR.swift

--- Volantes2/AR.swift
+++ Volantes2/AR.swift
@@ -7,27 +7,80 @@
 //
 
 import UIKit
+import ARKit
+import SceneKit
+import SpriteKit
+import CoreLocation
+//import Mapbox
+//import MapboxDirections
+//import Turf
 
 class AR: UIViewController {
+    
+    var automaticallyFindTrueNorth = true
+    let configuration = ARWorldTrackingConfiguration()
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+  //  @IBOutlet weak var mapView: MGLMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        startSession()
+    }
+    
+    private func startSession() {
+        
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configuration) //runs AR
+        self.sceneView.autoenablesDefaultLighting = true
 
-        // Do any additional setup after loading the view.
+
+        if automaticallyFindTrueNorth {
+            configuration.worldAlignment = .gravityAndHeading
+        } else {
+            configuration.worldAlignment = .gravity
+        }
+        
     }
     
     @IBAction func GoBack(_ sender: UIButton) {
         performSegue(withIdentifier: "arback", sender: self)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func addNode(_ sender: Any) {
+        
+//        let node =  createSphereNode(radius: 0.1, color: UIColor.red)
+//        let anchor = ARAnchor(name: <#T##String#>, transform: <#T##simd_float4x4#>) //look at demo app
+//        renderer(sceneView, didAdd: node, for: anchor)
+        
     }
-    */
-
+    
 }
+
+//Nodes Class
+
+extension AR: ARSCNViewDelegate {
+    
+    //creates a sphere node
+    func createSphereNode(radius: CGFloat, color: UIColor) -> SCNNode {
+        let sphere = SCNSphere(radius: radius)
+        sphere.firstMaterial?.diffuse.contents = color;
+        
+        let sphereNode = SCNNode(geometry: sphere)
+        
+        return sphereNode
+    }
+    
+    func addSphereNode(to node: SCNNode, for anchor: ARAnchor) {
+        let sphere = createSphereNode(radius: 0.2, color: UIColor.red)
+        node.addChildNode(sphere)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        let MBAnchor = anchor
+        addSphereNode(to: node, for: MBAnchor)
+    }
+    
+}
+
